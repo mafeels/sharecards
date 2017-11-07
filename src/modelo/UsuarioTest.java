fe09067ff7c2b937cc2f9383067c960ade0f38c8
Aluno <Aluno@I1-PC15-279348>
2017-11-07 16:41:42 -0200

    bla

---
+++ src/modelo/UsuarioTest.java
@@ -0,0 +1,14 @@
+package modelo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UsuarioTest {
+
+	@Test
+	public void testSenha() {
+		Usuario user = new Usuario();//blabla
+	}
+
+}
