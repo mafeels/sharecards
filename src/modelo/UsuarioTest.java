a1207fa4feab6afd6baa8e8fe84592012cdad4df
Aluno <Aluno@I1-PC15-279348>
2017-11-07 16:58:31 -0200

    Sucesso do teste da função checkSenha()da classeUsuario

--- src/modelo/UsuarioTest.java
+++ src/modelo/UsuarioTest.java
@@ -1,14 +1,14 @@
 package modelo;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public class UsuarioTest {
+	String primeiroNome, ultimoNome, username, dataNascimento, senha, email;
 
 	@Test
 	public void testSenha() {
-		Usuario user = new Usuario();//blabla
+		Usuario user = new Usuario("Will", "Masa", "WillMasa000", "12/4/01", "auguaargorua", "ieawmmf@mauifehna.asf");
+		Assert.assertEquals(true, user.checkSenha("auguaargorua"));
 	}
-
 }
