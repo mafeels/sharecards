8a867b28a50272d574df54b6b8ea3c38e965fca5
Aluno <Aluno@I1-PC16-279349>
2017-11-07 16:09:53 -0200

    criar classe edita

--- src/modelo/Edita.java
+++ src/modelo/Edita.java
@@ -1,5 +1,37 @@
 package modelo;
 
-public class Edita {
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
+public class Edita {
+	protected String dataEdita, codigoUsuario, codigoFlashcard;
+	
+	public Edita(String codigoUsuario, String codigoFlashcard) {
+		this.codigoUsuario = codigoUsuario;
+		this.codigoFlashcard = codigoFlashcard;
+		this.dataEdita = java.time.Instant.now().toString();
+	}
+
+	public String getDataEdita() {
+		return dataEdita;
+	}
+
+	public String getCodigoUsuario() {
+		return codigoUsuario;
+	}
+
+	public void setCodigoUsuario(String codigoUsuario) {
+		this.codigoUsuario = codigoUsuario;
+	}
+
+	public String getCodigoFlashcard() {
+		return codigoFlashcard;
+	}
+
+	public void setCodigoFlashcard(String codigoFlashcard) {
+		this.codigoFlashcard = codigoFlashcard;
+	}
+	
 }
