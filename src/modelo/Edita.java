ae12c97c2d54d77231ab7f44182b6c4ec5abb934
Aluno <Aluno@I1-PC16-279349>
2017-11-07 16:11:07 -0200

    haa correções

--- src/modelo/Edita.java
+++ src/modelo/Edita.java
@@ -1,9 +1,5 @@
 package modelo;
 
-import java.sql.Date;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 
 public class Edita {
 	protected String dataEdita, codigoUsuario, codigoFlashcard;
