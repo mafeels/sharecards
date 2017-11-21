7d5977d4fc2154a58b5618c338bc4364d3660bd3
WillMasa <will.masa@yahoo.com.br>
2017-11-21 08:01:52 -0200

    adws
    eaw

---
+++ FlashNotes Projeto/BDD - FlashNotes/MySQL Scripts FlashNotes/Database/database_flashnotes.sql
@@ -0,0 +1,70 @@
+create database flashnotes;
+drop database flashnotes;
+show databases;
+use flashnotes;
+show tables;
+create table usuario(
+	codigo_usuario int not null,
+    e_mail varchar(50) not null,
+    username varchar(20) not null,
+    data_nascimento date not null,
+    senha varchar(12) not null,
+    primeiro_nome varchar(20) not null,
+    ultimo_nome varchar(20) not null,
+    primary key(codigo_usuario, e_mail)
+    ) engine=innodb;
+
+create table flashcard(
+	codigo_usuario int not null,
+    e_mail varchar(50) not null,
+    codigo_flashcard int not null,
+    nome_flashcard varchar(30) not null,
+    frente_flashcard mediumtext not null,
+    verso_flashcard mediumtext not null,
+    autor_flashcard varchar(20) not null,
+    categoria_flashcard varchar(25) not null,
+    data_criacao datetime not null,
+    preco_flashcard int not null,
+    imagem_flashcard varchar(100) null,
+    primary key (codigo_flashcard, codigo_usuario, e_mail),
+    constraint fk_codigo_usuario foreign key(codigo_usuario)
+    references usuario(codigo_usuario),
+    constraint fk_e_mail foreign key(e_mail)
+    references usuario(e_mail)) engine=innodb;
+    
+
+create table edita(
+	data_edicao datetime,
+    codigo_flashcard int not null,
+    codigo_usuario int not null,
+    e_mail varchar(50) not null,
+	primary key (codigo_flashcard, codigo_usuario, e_mail),
+    constraint foreign key(codigo_flashcard)
+    references flashcard(codigo_flashcard),
+    constraint foreign key(codigo_usuario)
+    references flashcard(codigo_usuario),
+    constraint foreign key(e_mail)
+    references usuario(e_mail)) engine=innodb;
+    
+
+create table compra (
+	data_compra datetime,
+    valor_total_compra int not null,
+    codigo_flashcard int not null,
+    codigo_usuario int not null,
+    e_mail varchar(50) not null,
+	primary key (codigo_flashcard, codigo_usuario, e_mail),
+    constraint fk_codigo_flashcard_compra foreign key(codigo_flashcard)
+    references flashcard(codigo_flashcard),
+    constraint fk_codigo_usuario_compra foreign key(codigo_usuario)
+    references flashcard(codigo_usuario),
+    constraint fk_e_mail foreign key(e_mail)
+    references usuario(e_mail)) engine=innodb;
+show tables;
+	
+    
+drop table usuario;
+drop table flashcard;
+    
+	SHOW ENGINE INNODB STATUS;
+	
