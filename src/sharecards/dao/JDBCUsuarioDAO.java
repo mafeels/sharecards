0464f2f2e1d7cee25f22233c9399f6771a131c06
WillMasa <will.masa@yahoo.com.br>
2018-06-17 22:53:37 -0300

    JDBC CERTO

--- src/sharecards/dao/JDBCUsuarioDAO.java
+++ src/sharecards/dao/JDBCUsuarioDAO.java
@@ -41,15 +41,18 @@ public class JDBCUsuarioDAO implements UsuarioDAO {
 
 		// executa
 		System.out.println(" Executando comando ... ");
-		stmt.execute();
+		stmt.executeUpdate();
 		System.out.println("Fechando conexão ...");
 		stmt.close();
-		
-		PreparedStatement pstmt = conexao.prepareStatement("select codigo_usuario FROM usuario WHERE (senha = '?') AND (e_mail = '?')");
+		String cod = null;
+		PreparedStatement pstmt = conexao.prepareStatement("select codigo_usuario FROM usuario WHERE (senha = ?) AND (e_mail = ?)");
 		pstmt.setString(1, u.getSenha());
 		pstmt.setString(2, u.getEmail());
-		ResultSet rs = stmt.executeQuery();
-		String cod = rs.getNString("codigo_usuario");
+		ResultSet rs = pstmt.executeQuery();
+		if(rs != null && rs.next()){
+            cod = rs.getString("codigo_usuario");
+        } 
+		pstmt.close();
 		conexao.close();
 		
 		return cod; 
