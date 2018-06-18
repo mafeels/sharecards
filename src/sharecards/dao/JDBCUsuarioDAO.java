01725fa6d4c8514cd33c9ad4758ecb4524edeae8
mattalvesartist <mathew.eu@live.com>
2018-06-17 23:04:18 -0300

    aa

--- src/sharecards/dao/JDBCUsuarioDAO.java
+++ src/sharecards/dao/JDBCUsuarioDAO.java
@@ -44,6 +44,7 @@ public class JDBCUsuarioDAO implements UsuarioDAO {
 		stmt.executeUpdate();
 		System.out.println("Fechando conexão ...");
 		stmt.close();
+		
 		String cod = null;
 		PreparedStatement pstmt = conexao.prepareStatement("select codigo_usuario FROM usuario WHERE (senha = ?) AND (e_mail = ?)");
 		pstmt.setString(1, u.getSenha());
